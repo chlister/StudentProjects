79e66441ee952e86fdb2c13e7be3dfa19ffb6584
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-02-24 15:40:51 +0100

    Last comment added

--- H2/TestDBConn/TestDBConn/Program.cs
+++ H2/TestDBConn/TestDBConn/Program.cs
@@ -9,21 +9,26 @@ namespace TestDBConn
 {
     class Program
     {
+        // I am testing a connection to a remote MySQL server hosted on a linux machine
         static void Main(string[] args)
         {
+            // Connection string for our local database
             string cs = @"server=10.108.233.30;port=3306;userid=david;pwd=Dj110489;database=dummy";
             MySqlConnection conn = null;
             MySqlDataReader rdr = null;
             try
             {
                 conn = new MySqlConnection(cs);
+                // Opening the connection
                 conn.Open();
                 string stmt = "SELECT * FROM DummyTable";
+                // Testing if i have gotten the table and can select from it
                 MySqlCommand cmd = new MySqlCommand(stmt, conn);
+                // executing the select
                 rdr = cmd.ExecuteReader();
                 while (rdr.Read())
                 {
-
+                    // just need one piece of info to know that we're through
                     Console.WriteLine(rdr.GetString(0));
                 }
 
@@ -32,16 +37,18 @@ namespace TestDBConn
             {
                 Console.WriteLine($"Error: {e.ToString()}");
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                Console.WriteLine("Someething else");
+                Console.WriteLine(e.Message);
             }
             finally
             {
+                // Closing reader if it hasn't done so
                 if (rdr != null)
                 {
                     rdr.Close();
                 }
+                // Closing connection if it hasn't done so
                 if (conn != null)
                 {
                     conn.Close();
