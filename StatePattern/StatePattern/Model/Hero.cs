c1561eab2e1e4e9a4761a9f4a7980e5eb4726056
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-03-14 19:44:39 +0100

    State and event testing

---
+++ StatePattern/StatePattern/Model/Hero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.Model
+{
+    class Hero : IHero
+    {
+        public void Feed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StatePattern/StatePattern/Model/State/IState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.Model.State
+{
+    public interface IState : IHero
+    {
+        void SaveTheDay();
+        void BattleVillain();
+        void Eat();
+        void Sleep();
+    }
+}
