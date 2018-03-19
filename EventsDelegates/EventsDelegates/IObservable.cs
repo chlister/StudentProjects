122df123819320dd9bfe6db8a2abfad61916a7fb
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-03-19 22:13:22 +0100

    Need to figure out how to send values with eventargs without needing a custom eventargs

---
+++ EventsDelegates/EventsDelegates/IObservable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegates
+{
+    interface IObservable
+    {
+        /// <summary>
+        /// Event that can be subscribed to - Class that implement needs to invoke the event when changes happend
+        /// </summary>
+        event EventHandler ValueChanged;
+    }
+}
---
+++ EventsDelegates/EventsDelegates/Model/IHero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegates.Model
+{
+    interface IHero
+    {
+        int FoodNeed { get; set; }
+        int SleepNeed { get; set; }
+        void Feed(int food);
+        void Sleep();
+    }
+}
