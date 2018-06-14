616fefb422290e02cf5ea9d4ead0163e14be667d
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-06-14 12:33:17 +0200

    Project start

---
+++ Intrest_Projects/ArenaRPG/ArenaRPG/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using ArenaRPG.Annotations;
+
+namespace ArenaRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            
+            for (int i = 0; i < 100; i++)
+            {
+                counter++;
+                if (counter == 50)
+                {
+                    
+                }
+            }
+
+            Console.WriteLine(counter);
+
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+
+        
+    }
+
+    public class Counter 
+    {
+        public event EventHandler ThresholdReached;
+
+        protected virtual void OnThresholdReached(EventArgs e)
+        {
+            EventHandler handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+
+    public class Display
+    {
+        public Display()
+        {
+
+        }
+    }
+}
