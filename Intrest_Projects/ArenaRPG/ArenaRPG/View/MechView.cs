ff860495c175a3c9e636ee4b977dab6a2c999e5d
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-06-18 14:04:39 +0200

    Added a view folder plus first view

---
+++ Intrest_Projects/ArenaRPG/ArenaRPG/View/MechView.cs
@@ -0,0 +1,33 @@
+﻿using ArenaRPG.Model.MechModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaRPG.View
+{
+    public class MechView
+    {
+        public IMech Mech { get; set; }
+        public MechView(IMech mech)
+        {
+            Mech = mech;
+        }
+
+        public void ShowMechParts()
+        {
+            // Show all parts of the mech - remember some parts may be missing
+            Console.WriteLine("Mech overview:");
+            foreach (var part in Mech.Parts)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(part.PartName + "\nIs Functional: "+part.IsActive);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
