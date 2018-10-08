f2a020c96b37498edc1c96d1192426e2822327d8
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-10-08 10:50:33 +0200

    Added directioneventargs + direction enum

---
+++ H3/CustomControlInput/DirectionEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomControlInput
+{
+public    class DirectionEventArgs : EventArgs
+    {
+        public Directions Direction { get; set; }
+        public DirectionEventArgs(Directions dir)
+        {
+            Direction = dir;
+        }
+    }
+    public enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
