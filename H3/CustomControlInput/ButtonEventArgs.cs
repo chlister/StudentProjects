b5e203ae47ed5f262ea0280f9ce0c7572c0873e5
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-10-08 10:49:28 +0200

    Added buttoneventargs + button enums

---
+++ H3/CustomControlInput/ButtonEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomControlInput
+{
+    public class ButtonEventArgs<T> : EventArgs  
+    {
+        public T Button { get;}
+        public ButtonEventArgs(T button)
+        {
+            Button = button;
+        }
+    }
+    public enum FryButtons
+    {
+        B0,
+        B1,
+        B2
+    }
+    public enum PotButtons
+    {
+        B0,
+        B1
+    }
+    public enum CuttingButtons
+    {
+        B0,
+        B1,
+        B2,
+        B3
+    }
+    public enum AssemblerButtons
+    {
+        B0,
+        B1
+    }
+}
