cb8a5f6b9ba673a437b8f4b7ed65efacd090cb94
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-10-08 10:49:47 +0200

    Added poteventargs + potstate enum

---
+++ H3/CustomControlInput/PotEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomControlInput
+{
+    class PotEventArgs :EventArgs
+    {
+        public PotState State { get; set; }
+        public PotEventArgs(PotState state)
+        {
+            State = state;
+        }
+    }
+    public enum PotState
+    {
+        LidOn,
+        LidOff
+    }
+}
