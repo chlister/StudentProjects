983f0be0f6fec058c135e14c550552af7de16513
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-10-08 10:50:09 +0200

    Added fryeventargs + frystate enum

---
+++ H3/CustomControlInput/FryEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomControlInput
+{
+public    class FryEventArgs : EventArgs
+    {
+        public FryState State { get; set; }
+        public FryEventArgs(FryState state)
+        {
+            State = state;
+        }
+    }
+    public enum FryState
+    {
+        FryOff,
+        FryOn
+    }
+}
