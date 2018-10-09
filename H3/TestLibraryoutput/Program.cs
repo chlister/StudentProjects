d197a8e67332c74d4976b2c5628c3e3fc0a81c0e
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2018-10-09 10:35:52 +0200

    Added test for testing library - to be roemoved later

---
+++ H3/TestLibraryoutput/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomControl;
+using CustomControl.Input;
+using CustomControl.Event;
+
+
+namespace TestLibraryoutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectSerial cs = new ConnectSerial("COM6", 9600);
+            cs.CuttingButtonPressed += Cs_CuttingButtonPressed;
+            cs.OpenConnection();
+
+            while (true)
+            {
+
+            }
+        }
+
+        private static void Cs_CuttingButtonPressed(object sender, ButtonEventArgs<CuttingButtons> bea)
+        {
+            Console.WriteLine(bea.Button);
+        }
+    }
+}
